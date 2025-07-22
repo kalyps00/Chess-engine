154b36605d24641aab7707b89d39725658f6cd00
Kamil <kpawlik2005@gmail.com>
2025-07-22 14:45:05 +0200

    Small changes to last commit -test main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "=== CHESS ENGINE DEBUG TEST ===" << std::endl;
+
+    int testVariable = 42;
+
+    std::cout << "Test variable: " << testVariable << std::endl;
+    std::cout << "Debugger setup complete!" << std::endl;
+
+    return 0;
+}
