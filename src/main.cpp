41d94687695ced28e1fd1172c5cbaf8766df2190
Kamil <kpawlik2005@gmail.com>
2025-07-22 17:18:00 +0200

    Added: - Pieces types and constants in Types.hpp - Almost complete board representation

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,7 @@
+#include "GameControler.hpp"
+#include <SFML/Graphics.hpp>
 #include <iostream>
 
 int main()
 {
-    std::cout << "=== CHESS ENGINE DEBUG TEST ===" << std::endl;
-
-    int testVariable = 42;
-
-    std::cout << "Test variable: " << testVariable << std::endl;
-    std::cout << "Debugger setup complete!" << std::endl;
-
-    return 0;
 }
---
+++ src/model/Board.hpp
@@ -0,0 +1,47 @@
+#ifndef BOARD_HPP
+#define BOARD_HPP
+#include <string>
+#include "Move.hpp"
+#include <Types.hpp>
+class Board
+{
+private:
+    // Engine part - bitboards
+
+    //  white pices bitboards
+    long long
+        white_pawns,
+        white_knights,
+        white_bishops,
+        white_rooks,
+        white_queens,
+        white_king;
+    // black pices bitboards
+    long long
+        black_pawns,
+        black_knights,
+        black_bishops,
+        black_rooks,
+        black_queens,
+        black_king;
+    // Additional bitboards
+    long long
+        white_pieces,
+        black_pieces,
+        all_pieces,
+        empty_squares;
+    // Game state
+    bool white_to_move;
+
+    // View part
+    int board_arr[64];
+    int get_piece_at(int pos);
+
+public:
+    Board();
+    void load_fen_position(std::string fen);
+    void make_move();
+    void undo_move();
+    bool get_current_player();
+};
+#endif
